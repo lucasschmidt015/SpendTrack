1eff82bda5effb0dfdfed2a5c48aa8595cf1d14f
GeovaneMachado <geovanemachado9@gmail.com>
2023-04-16 19:43:09 -0300

    Criado page de dashboard para importar framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './Global.css';
+import Dashboard from "./Pages/Dashboard";
 
 
 export default function App(){
   return(
     <div>
       <h1>Hello World</h1>
+        <Dashboard />
     </div>
   );
 }
