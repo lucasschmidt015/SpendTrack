baa5d55846407c9670dd992eaa576d9c75a1b2ee
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-04-21 18:52:52 -0300

    Implementando a tela de login.

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,13 +1,23 @@
 import "./Login.css"
 import BgImg from "../../Resources/BackgroundLogin.jpg"
+import { BsChevronDown } from 'react-icons/bs';
 
 export default function Login(){
     return(
         <div className="loginbox">
             <div className="BackgroundImage">
                 <img src={BgImg} alt=""/>
+                <div className="first-line">
+                   <h3>Spend Track Sinten</h3>
+                   <div className="rodapes">
+                        <label>Pages<BsChevronDown/></label>
+                        <label>Authentication<BsChevronDown/></label>
+                        <label>Ecommerce<BsChevronDown/></label>
+                        <label>Docs<BsChevronDown/></label>
+                   </div>
+                   <button>BUYNOW</button>
+                </div>
             </div>
-            <h1>Hello World</h1>
         </div>
     );
 }
