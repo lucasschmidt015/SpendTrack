c342d4cb8de85dcfb3e0c328989204449e69b99a
lucas.schmidt <lucas.schmidt@gamatec.com.br>
2023-05-22 16:09:58 -0300

    Configuração para usar o console do redux.

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './Models/rootReducer';
 import rootSaga from './Models/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer,applyMiddleware(sagaMiddleware));
+// const store = createStore(rootReducer,applyMiddleware(sagaMiddleware));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 
