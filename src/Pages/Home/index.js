5a0b8f6510ac43dc59e401a40db37fcbb0c939b3
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-05-30 00:35:52 -0300

    Seguindo na implementação da tela de dashboard.

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,7 +3,13 @@ import "./Home.css"
 
 import Sidebar from '../../Components/Sidebar';
 
+import { MdOutlineFilterList } from 'react-icons/md';
+
 export default function Home() {
+
+  const handleNew = () => {
+    console.log('Handle new <----------');
+  }
 
  return (
    <div className='home-container'>
@@ -12,18 +18,30 @@ export default function Home() {
           <h1>Dashboard page</h1>
           <div className='line-dashboard'>
             <div className='dash-model'>
-
+              <div className='title-model'>
+                <h2>Earnings / Expenses</h2>
+                <MdOutlineFilterList/>
+              </div>
             </div>
             <div className='dash-model'>
-
+              <div className='title-model'>
+                <h2>Objectives</h2>
+                <button onClick={handleNew}>New</button>
+              </div>
             </div>
           </div>
           <div className='line-dashboard'>
             <div className='dash-model'>
-
+              <div className='title-model'>
+                <h2>Main Expenses</h2>
+                <MdOutlineFilterList/>
+              </div>
             </div>
             <div className='dash-model'>
-
+              <div className='title-model'>
+                <h2>Year-End Forecast</h2>
+                <MdOutlineFilterList/>
+              </div>
             </div>
           </div>
         </div>
