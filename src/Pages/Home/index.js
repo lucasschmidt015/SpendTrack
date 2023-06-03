4148738b7cc3015006a839f6f7a68e286b560bc3
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-06-03 17:07:54 -0300

    Implementando aba de objetivos.

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,8 +4,23 @@ import "./Home.css"
 import Sidebar from '../../Components/Sidebar';
 
 import { MdOutlineFilterList } from 'react-icons/md';
+import { useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
+import { AiFillEdit } from 'react-icons/ai';
 
 export default function Home() {
+
+  const obj = [
+    {id: 1, title: 'Viagem para São Fransisco', percentageAchieved: '50%', valueAchieved: 'R$ 6.000,00', valueObjective: 'R$ 12.000,00'},
+    {id: 2, title: 'Los Anjeles', percentageAchieved: '12%', valueAchieved: 'R$ 2.400,00', valueObjective: 'R$ 20.000,00'},
+    {id: 3, title: 'Londres', percentageAchieved: '100%', valueAchieved: 'R$ 10.000,00', valueObjective: 'R$ 10.000,00'},
+    {id: 4, title: 'Londres', percentageAchieved: '100%', valueAchieved: 'R$ 10.000,00', valueObjective: 'R$ 10.000,00'},
+    {id: 5, title: 'Londres', percentageAchieved: '100%', valueAchieved: 'R$ 10.000,00', valueObjective: 'R$ 10.000,00'},
+    {id: 6, title: 'Londres', percentageAchieved: '100%', valueAchieved: 'R$ 10.000,00', valueObjective: 'R$ 10.000,00'},
+    {id: 7, title: 'Londres', percentageAchieved: '100%', valueAchieved: 'R$ 10.000,00', valueObjective: 'R$ 10.000,00'},
+  ]
+
+  const [objectives, setObjectives] = useState(obj);
 
   const handleNew = () => {
     console.log('Handle new <----------');
@@ -27,6 +42,23 @@ export default function Home() {
               <div className='title-model'>
                 <h2>Objectives</h2>
                 <button onClick={handleNew}>New</button>
+              </div>
+              <div className='render-area-obejectives'>
+                <div className='scroll-area-objectives'>
+                  {objectives.map((object) => {
+                    return(
+                      <div className='objectives' key={object.id}>
+                        <p id='Title'>{object.title}</p>
+                        <p id='Percent'>{object.percentageAchieved}</p>
+                        <p id='Achieved'>{object.valueAchieved} / {object.valueObjective}</p>
+                        <div className='options-buttons'>
+                          <AiFillEdit/>
+                          <FaTrash/>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
